db3cd5b95453d3acc12cbd0a327caa90b0f878e6
Pikachu1902 <38290450+Pikachu1902@users.noreply.github.com>
2018-05-17 12:44:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,9 +8,6 @@ client.on('ready', () => {
 client.on('message', message => {
 if (message.content === 'ping') {
 message.reply('pong');
-}
-    
-client.on('message', message => {
 if (message.content === 'arschloch') {
 message.reply('selber!');
 }
